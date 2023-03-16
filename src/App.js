7b0c438fc884553bd6d0a9ea1c7e26835e115ac5
roy19920801 <legendej123@gmail.com>
2023-03-15 19:44:56 -0700

    compiler

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Compiler from "./Components/Compiler";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Compiler />
+      </>
+    );
+  }
+}
